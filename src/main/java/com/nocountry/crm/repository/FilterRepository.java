65ed50adeadf4028e0ab921c51eb95139f6d9725
sergprl <speralmoreno@gmail.com>
2025-11-26 09:51:42 -0600

    minor changes to filter repo

--- src/main/java/com/nocountry/crm/repository/FilterRepository.java
+++ src/main/java/com/nocountry/crm/repository/FilterRepository.java
@@ -3,11 +3,13 @@ package com.nocountry.crm.repository;
 import com.nocountry.crm.entity.Filter;
 import com.nocountry.crm.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface FilterRepository extends JpaRepository<Filter, UUID> {
     List<Filter> findByCompanyId(UUID companyId);
     Optional<Filter> findByCode(String code);
